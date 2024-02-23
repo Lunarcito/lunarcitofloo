cc3200a30ec42f6af7c30e1d811b2eaf8ab83917
Lunarcito <calle.carolina@gmail.com>
2024-02-23 23:59:30 +0100

    Set up ChakraProvider in App

--- lunarcitofloo/src/App.js
+++ lunarcitofloo/src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import Home from './pages/Home';
+import './App.css'
+import Home from './screens/Home'
+import { ChakraProvider} from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Home/>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+          <Home/>
+      </div>
+    </ChakraProvider>
   );
 }
 
