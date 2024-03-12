e09cafa4b1a4c9c3418d043dea828337e9e12f82
Lunarcito <calle.carolina@gmail.com>
2024-03-12 14:04:43 +0100

    Set up Login page

--- lunarcitofloo/src/screens/Login.js
+++ lunarcitofloo/src/screens/Login.js
@@ -1,145 +1,101 @@
-import React from "react";
-import React, { useState, useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react"
+import "../screens/Login.css"
 import {
-   Flex,
-   Heading,
-   Input,
-   Button,
-   InputGroup,
-   Stack,
-   InputLeftElement,
-   chakra,
-   Box,
-   Link,
-   FormControl,
-   FormHelperText,
-   InputRightElement
- } from "@chakra-ui/react"
-import { FaUserAlt, FaLock } from "react-icons/fa"
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
-import "../pages/Login.css"
- 
+    Flex,
+    Heading,
+    Input,
+    Button,
+    InputGroup,
+    Stack,
+    InputLeftElement,
+    chakra,
+    Box,
+    Link,
+    FormControl,
+    FormHelperText,
+    InputRightElement
+  } from "@chakra-ui/react";
+  import { FaUserAlt, FaLock } from "react-icons/fa";
+
+
 const CFaUserAlt = chakra(FaUserAlt);
 const CFaLock = chakra(FaLock);
 
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
 
-export default function Login(props) {
-    const { storeToken, storeFleetID, authenticateUser } = useContext(AuthContext);
-    const [user, setUser] = useState({
-        email: '',
-        password: ''
-    });
-    
-    const [showPassword, setShowPassword] = useState(false);
-    const [errorMessage, setErrorMessage] = useState(undefined);
-    const handleShowClick = () => setShowPassword(!showPassword);
-    const navigate = useNavigate();
-    props.funcNav(false)
+  const handleShowClick = () => setShowPassword(!showPassword);
 
-    const { t } = useTranslation();
-
-    const handleChange = (e) => {
-        setUser(prev => {
-            return {
-            ...prev,
-            [e.target.name]: e.target.value
-            }
-        })
-    }
-
-    const logInUser =()=>{
-        signInWithEmailAndPassword(auth, user.email, user.password)
-      .then((userCredential) => {
-        // LogIn 
-        const user = userCredential.user;
-        storeToken(user.accessToken);
-        requestFleetID(user.uid)
-        authenticateUser();
-        navigate('/')
-        props.funcNav(true)
-      })
-      .catch((error) => {
-        setErrorMessage(t('login.invalidpassword'))
-      });
-      }
-      const handleSubmit = async (e) => {
-        e.preventDefault()
-        logInUser() 
-    }
-    
-    return (
-    <div>
-        <form onSubmit={handleSubmit}>
-        <Flex 
-        flexDirection="column"
-        width="100wh"
-        height="100vh"
-        justifyContent="center"
-        alignItems="center"
-        >
-        <Stack
+  return (
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
         flexDir="column"
-        mb="30"
+        mb="5"
         justifyContent="center"
         alignItems="center"
-        >
-            <Heading className="Lunarcitofloo"/>
-            <Box minW={{ base: "80%", md: "468px" }}>
-                <Stack
-                spacing={4}
-                p="3rem"
-                boxShadow="md"
-                >
-                <FormControl>
-                    <InputGroup>
-                        <InputLeftElement
-                            pointerEvents="none"
-                            children={<CFaUserAlt color="gray.300" />}                  
-                        />
-                        <Input type="email" name="email" placeholder={t('login.email')} value={user.name} onChange={handleChange}/>
-                    </InputGroup>
-                </FormControl>
-                <FormControl>
-                    <InputGroup>
-                        <InputLeftElement
-                        pointerEvents="none"
-                        children={<CFaLock color="gray.300" />}                   
-                        />
-                        <Input
-                        name="password" 
-                        type={showPassword? "text" :"password"}
-                        placeholder={'login.password'}
-                        value={user.password} 
-                        onChange={handleChange} 
-                        />
-                        <InputRightElement width="4.5rem">
-                        <Button size="md" onClick={handleShowClick}> 
-                        {showPassword? 'login.hide':'login.show'}
-                        </Button>
-                        </InputRightElement>
-                    </InputGroup>
-                    <FormHelperText textAlign="right">
-                        <Link href='/' color="#1CE783">
-                        {'login.forgotpassword'}
-                        </Link>
-                        {errorMessage && <p className="error-Message">{errorMessage}</p>}
-                    </FormHelperText>
-                </FormControl>
-                <Button
-                    type="submit"
-                    colorScheme='gandolapp'
-                > 
-                {t('login.login')}
-                </Button>
-                <Link textAlign="center" href='/signup' color="#1CE783">
-                {'login.signup'}
-                </Link>
-                </Stack>
-            </Box>
-        </Stack>
-        </Flex>
-        </form>
-    </div>
-   )
-}
+      >
+        <Heading color="teal.300">Lunarcito Foodie</Heading>
+        <Box minW={{ base: "80%", md: "468px" }}>
+          <form>
+            <Stack
+              spacing={4}
+              p="3rem"
+              boxShadow="md"
+            >
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<CFaUserAlt color="gray.300" />}
+                  />
+                  <Input type="email" placeholder="email address" />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    color="gray.300"
+                    children={<CFaLock color="gray.300" />}
+                  />
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                      {showPassword ? "Hide" : "Show"}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                <FormHelperText textAlign="right">
+                  <Link>forgot password?</Link>
+                </FormHelperText>
+              </FormControl>
+              <Button
+                borderRadius={4}
+                type="submit"
+                colorScheme="teal"
+              >
+                Login
+              </Button>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+      <Box>
+        <Link color="teal.300" href="/signup">
+          Sign Up
+        </Link>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Login;
